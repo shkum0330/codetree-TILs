860205c4dcb6e3841707082a2631af3607c1efb9
sehyun kum <102662024+shkum0330@users.noreply.github.com>
2024-03-29 22:00:14 +0900

    Title: 바이러스 백신, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240329/바이러스 백신/vaccine-for-virus.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int res=987654321;
+bool flag=false;
+int mmap[51][51];
+int dist[51][51];
+bool visited[51][51];
+int dy[4]={-1,0,1,0};
+int dx[4]={0,-1,0,1};
+vector<pair<int,int>> hospital;
+vector<int> v;
+
+bool check(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(mmap[i][j]==1 || mmap[i][j]==2) continue;
+            else if(!visited[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+void bfs(int y,int x){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            visited[i][j]=false;
+        }
+    }
+    queue<pair<int,int>> q;
+    q.push({y,x});
+    visited[y][x]=true;
+    dist[y][x]=0;
+    while(!q.empty()){
+        int y=q.front().first;
+        int x=q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int ny=y+dy[i];
+            int nx=x+dx[i];
+
+            if(ny>=n || ny<0 || nx>=n || nx<0) continue;
+            
+            if(!visited[ny][nx] && mmap[ny][nx]!=1){
+                // cout<<ny<<' '<<nx<<'\n';
+                if(dist[ny][nx]==0) dist[ny][nx]=dist[y][x]+1;
+                else dist[ny][nx]=min(dist[y][x]+1,dist[ny][nx]);
+                q.push({ny,nx});
+                visited[ny][nx]=true;
+            }
+        }
+    }
+
+    
+
+}
+void combi(int idx){
+
+    if(v.size()==m){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dist[i][j]=0;
+            }
+        }
+        for(int i=0;i<m;i++){
+            bfs(hospital[v[i]].first,hospital[v[i]].second);
+        }
+
+        for(int i=0;i<hospital.size();i++){
+            dist[hospital[v[i]].first][hospital[v[i]].second]=0;
+        }
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dist[i][j]<<' ';
+        //     }
+        //     cout<<'\n';
+        // }
+        // cout<<'\n';
+        if(check()){
+            flag=true;
+            int tmp=0;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(mmap[i][j]==1) continue;
+                    tmp=max(tmp,dist[i][j]);
+                }
+            }
+
+            res=min(res,tmp);
+        }
+        return;
+    }
+    for(int i=idx+1;i<hospital.size();i++){
+        // cout<<i<<'\n';
+        v.push_back(i);
+        combi(i);
+        v.pop_back();
+    }
+}
+int main() {
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> mmap[i][j];
+            if(mmap[i][j]==2) hospital.push_back({i,j});
+        }
+    }
+
+    for(int i=0;i<=hospital.size()-m;i++){
+        v.push_back(i);
+        combi(i);
+        v.pop_back();
+    }
+    if(!flag) cout<<-1<<'\n';
+    else cout<<res<<'\n';
+}
